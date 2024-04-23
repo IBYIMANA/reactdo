b5723b63d39403da9bd8518fcc7270136ace0d7b
IBYIMANA <filletteibyimana@gmail.com>
2024-04-23 11:48:30 +0200

    seco

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
+
 import Home from "./pages/Home";
 import About from './pages/About';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
 
   return (
     <>
+       <Navbar/>
       <Router>
          <Routes>
          
            <Route path=""  element={< Home />} />
            <Route path="/About"  element={<About/>} />
            <Route path='/Contact' element={<Contact/> }/>
+           
          </Routes>
         </Router>
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-green-300 w-full h-16 text-blue-800'>
+    <h1 className='ml-8 text-2xl'>Fillette Theodette </h1>
+    </div>
+   
+  )
+}
+
+export default Navbar
