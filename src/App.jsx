a04f96be5cdd77ad5228cae31539eee031c1be63
IBYIMANA <filletteibyimana@gmail.com>
2024-04-23 10:52:34 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css'
+import Home from "./pages/Home";
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+
+  return (
+    <>
+      <Router>
+         <Routes>
+         
+           <Route path=""  element={< Home />} />
+           <Route path="/About"  element={<About/>} />
+           <Route path='/Contact' element={<Contact/> }/>
+         </Routes>
+        </Router>
+    </>
+  )
+}
+
+export default App
